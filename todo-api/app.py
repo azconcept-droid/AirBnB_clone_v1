aefb820b3580f12caaaea23d717e872c2115150c
Yahaya <azconcept2016@gmail.com>
2022-10-21 10:36:11 +0100

    feat: final code for RESTful API with Python and Flask tutotrial :sparkles:

--- todo-api/app.py
+++ todo-api/app.py
@@ -2,6 +2,9 @@
 
 from flask import abort, Flask, jsonify
 from flask import make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
 
 app = Flask(__name__)
 
@@ -27,6 +30,7 @@ def index():
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
     return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
@@ -94,6 +98,18 @@ def make_public_task(task):
         else:
             new_task[field] = task[field]
     return new_task
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'azeez':
+        return 'python' #password
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
 if __name__ == '__main__':
