359addeca9e31ed609a9950e5519bf2bb303e460
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-05 18:48:47 +0100

    feat: add basemodel methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Base model module for all classes"""
+
+import uuid
+import datetime
+
+class BaseModel():
+    """This is base model class"""
+
+    id = str(uuid.uuid4())
+    created_at = datetime.now
+    updated_at = datetime.now
+
+    def save(self):
+        """Updates public instance attribute updated_at with current date time"""
+        updated_at = datetime.now
+    def to_dict(self):
+        """returns a dict containing all keys/values of __dict__ of the instance"""
+        
+    def __str__():
+        """print class name and args"""
+        print('[]()<>')
