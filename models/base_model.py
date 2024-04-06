acc3285f1aab1c9a6eb9d821ea23d2683fdfaab9
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-06 13:21:50 +0100

    feat: implement save to_dict and __str__ methods

--- models/base_model.py
+++ models/base_model.py
@@ -2,21 +2,31 @@
 """Base model module for all classes"""
 
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel():
     """This is base model class"""
 
-    id = str(uuid.uuid4())
-    created_at = datetime.now
-    updated_at = datetime.now
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """Updates public instance attribute updated_at with current date time"""
-        updated_at = datetime.now
+        self.updated_at = datetime.now()
+
     def to_dict(self):
         """returns a dict containing all keys/values of __dict__ of the instance"""
+        instance = self.__dict__.copy()
+        instance['__class__'] = self.__class__.__name__
+        instance['created_at'] = str(instance['created_at'])
+        instance['updated_at'] = str(instance['updated_at'])
+
+        return instance
+
         
-    def __str__():
-        """print class name and args"""
-        print('[]()<>')
+    def __str__(self):
+        """print object to stdout"""
+        cls = self.__class__.__name__
+        return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
