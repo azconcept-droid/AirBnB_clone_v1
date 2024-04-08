c685410b56de004060e77f37c7ac79006e1f31b9
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-08 15:11:42 +0100

    add kwargs for deserialization

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,14 @@ from datetime import datetime
 class BaseModel():
     """This is base model class"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None:
+            for key, value in kwargs.iteritems():
+                print(key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Updates public instance attribute updated_at with current date time"""
